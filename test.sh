b5ec2690cd7f935b65bef9f3e2818bf320094bf8
chfw <wangc_2011@hotmail.com>
2018-09-14 23:14:53 +0100

    :green_heart: fix travis build

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 pip freeze
-nosetests --with-coverage --cover-package pyexcel_xlsxr --cover-package tests tests --with-doctest --doctest-extension=.rst README.rst docs/source pyexcel_xlsxr && flake8 . --exclude=.moban.d,docs --builtins=unicode,xrange,long
+nosetests --with-coverage --cover-package pyexcel_xlsxr --cover-package tests tests --with-doctest --doctest-extension=.rst README.rst  pyexcel_xlsxr && flake8 . --exclude=.moban.d,docs --builtins=unicode,xrange,long
